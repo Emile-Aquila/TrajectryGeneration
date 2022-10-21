1630cfdc0a138f6c0d55ac434a1be99eb681ce38
Emile-Aquila <tmk8oakan6130@gmail.com>
2022-10-21 17:34:27 +0900

    add model collision check in MC_MPC

--- MC_MPC.py
+++ MC_MPC.py
@@ -6,7 +6,6 @@ from models.Robot_model import RobotState, RobotModel_with_Dynamics
 from models.DynamicsModel import V_Omega, V_Omega_Config, Parallel_TwoWheel_Vehicle_Model
 from typing import Any
 from A_star import A_star
-from utils.utils import spline
 
 
 class MCMPC_Config:
@@ -14,7 +13,7 @@ class MCMPC_Config:
         self.act_config = V_Omega_Config(max_v=1.0, max_omega=90.0 * math.pi / 180.0,
                                          max_d_v=8.0, max_d_omega=180.0 * math.pi / 180.0,
                                          sigma_v=0.9, sigma_omega=90.0 * math.pi / 180.0 * 0.9, dt=0.1)
-        self.vel_weight = [1.0, 0.0]
+        self.vel_weight = [1.0, -0.05]
         self.d_vel_weight = [1.0, 1.0]
         self.predict_step_num = 10  # 1回の予測に用いるステップ数
         self.iteration_num = 40  # 1回の制御周期あたりの予測計算の回数
@@ -37,6 +36,8 @@ class MCMPC:
 
     def _calc_nearest_index(self, point: Point2D, global_path: list[Point2D], start_index: int) -> int:
         min_dist = float("inf")
+        if self._check_reach_point(point, global_path, start_index):
+            start_index = min(start_index+2, len(global_path)-1)
         min_idx = start_index
         for i in range(start_index, min(len(global_path), start_index + self.config.id_search_num)):
             if min_dist > (global_path[i] - point).len():
@@ -44,30 +45,33 @@ class MCMPC:
                 min_idx = i
         return min_idx
 
-    def _check_reach_point(self, state: RobotState, global_path: list[Point2D], global_path_id: int) -> bool:
-        if (global_path[global_path_id] - state.pos).len() < self.config.reach_dist:
+    def _check_reach_point(self, pos: Point2D, global_path: list[Point2D], global_path_id: int) -> bool:
+        if (global_path[global_path_id] - pos).len() < self.config.reach_dist:
             return True
         else:
             return False
 
-    def _calc_eval_func(self, state: RobotState, trajectory: list[RobotState], global_path: list[Point2D],
-                        global_path_idx: int) -> float:
+    def _eval_trajectory(self, state: RobotState, trajectory: list[RobotState], global_path: list[Point2D],
+                         global_path_idx: int) -> float:
         traj = np.insert(np.array(trajectory), 0, state)
         vels = np.insert(np.array([stat.vel for stat in trajectory]), 0, state.vel)
         d_vels = np.abs(vels[1:-1] - vels[0:-2]) * (1.0 / self.config.act_config.dt)
 
+        score = 0.0
         for stat in traj:
             if self.field.check_collision(stat.pos):
                 return -100.0 * len(traj)
-        score = 0.0
+        for stat in traj:
+            if self.model.check_collision(stat, field.obstacles):
+                score -= 1.0
         ave_d_vel = np.sum(d_vels) * (1.0 / len(d_vels))
         ave_vel = np.sum(vels) * (1.0 / len(vels))
         path_id = global_path_idx
 
         for i, tmp in enumerate(traj):
-            path_id = min(self._calc_nearest_index(tmp.pos, global_path, path_id), len(global_path) - 1)
-            score -= (global_path[path_id] - tmp.pos).len() + ave_d_vel.weighted_sum(self.config.d_vel_weight) * 0.003 \
-                     - ave_vel.weighted_sum(self.config.vel_weight) * 0.001
+            path_id = min(self._calc_nearest_index(tmp.pos, global_path, global_path_idx), len(global_path) - 1)
+            score -= (global_path[path_id] - tmp.pos).len()*0.4 + ave_d_vel.weighted_sum(self.config.d_vel_weight) * 0.003 \
+                     - ave_vel.weighted_sum(self.config.vel_weight) * 0.003
             if path_id > 0:
                 target_vec = global_path[path_id] - global_path[path_id - 1]
                 d_angle = tmp.pos.theta - math.atan2(target_vec.y, target_vec.x)
@@ -78,7 +82,6 @@ class MCMPC:
                         d_angle += math.pi * 2.0
                 d_angle = min(abs(d_angle), math.pi * 2.0 - abs(d_angle))
                 score -= d_angle * 0.1
-        # print("score {}".format(score))
         return score
 
     def _predict_trajectory(self, state: RobotState, act_pre) -> tuple[list[RobotState], list]:
@@ -104,7 +107,7 @@ class MCMPC:
             traj, acts = self._predict_trajectory(state, act_pre)
             actions.append(acts)
             trajectories.append(traj)
-            scores.append((self._calc_eval_func(state, traj, global_path, global_path_idx), i))
+            scores.append((self._eval_trajectory(state, traj, global_path, global_path_idx), i))
         scores.sort()
         scores.reverse()
         ans_scores = np.array([score for score, _ in scores[0:self.config.num_trajectories_for_calc]])
@@ -119,14 +122,15 @@ class MCMPC:
         state = copy.deepcopy(initial_state)
         act = copy.deepcopy(act_pre)
         ans = [state.pos]
+        global_path_idx = 0
         for _ in range(1000):
-            idx = self._calc_nearest_index(state.pos, global_path, 0)
-            act, predictive_path = self.calc_step(state, act, global_path, min(idx + 1, len(global_path) - 1))
+            global_path_idx = self._calc_nearest_index(state.pos, global_path, global_path_idx)
+            act, predictive_path = self.calc_step(state, act, global_path, min(global_path_idx, len(global_path) - 1))
             state = self.model.step(state, act, self.config.act_config.dt)
             ans.append(state.pos)
-            if self._check_reach_point(state, global_path, len(global_path) - 1):
+            if self._check_reach_point(state.pos, global_path, len(global_path) - 1):
                 break
-            self.field.plot_anime(ans, start_point, target_point, global_path, [tmp.pos for tmp in predictive_path])
+            self.field.plot_anime(ans[-min(20, len(ans)):-1], start_point, target_point, global_path, [tmp.pos for tmp in predictive_path])
         return ans
 
 
@@ -136,16 +140,16 @@ if __name__ == '__main__':
 
     start_point = Point2D(0.5, 0.5)
     target_point = Point2D(8.0, 8.0)
-    dist, path_global_pre = A_star(field, start_point, target_point, None, check_length=0.1, unit_dist=0.2, show=True)
+    r_model = Parallel_TwoWheel_Vehicle_Model([Circle(x=0.0, y=0.0, r=0.1)])
+    dist, path_global_pre = A_star(field, start_point, target_point, r_model, check_length=0.1, unit_dist=0.2, show=True)
 
     print(dist)
-    path_global = path_global_pre[::4]
+    path_global = path_global_pre
     path_global.append(path_global_pre[-1])
     field.plot_path(path_global, start_point, target_point, show=True)
 
     mcmpc_config = MCMPC_Config()
     init_state = RobotState(Point2D(start_point.x, start_point.y, math.pi / 2.0), V_Omega(0.0, 0.0))
-    r_model = Parallel_TwoWheel_Vehicle_Model([Circle(x=0.0, y=0.0, r=0.1)])
 
     # path_glob = spline(path_global) # スプライン補間
     field.plot_path(path_global, start_point, target_point, show=True)
